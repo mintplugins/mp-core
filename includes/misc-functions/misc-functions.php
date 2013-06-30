0be4540a0042227cac587b33ee22ed6fca8f64bb
Phil Johnston <johnstonphilip@gmail.com>
2013-06-30 00:52:53 -0400

    Bug fix for temp WP bug fix on 404 pages
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -13,13 +13,15 @@ add_action( 'wp_enqueue_scripts', 'mp_core_enqueue_scripts' );
  * The function below is a temporary fix for this bug: http://core.trac.wordpress.org/ticket/18614
  */
 function wordpress_fix_bug( $wp_query ) {
-    if ( $wp_query->is_post_type_archive && $wp_query->is_tax ) {
-        global $post_type_obj;
-        $wp_query->is_tax = false;
-        $post_type_obj = get_queried_object();
-        if (empty($post_type_obj->labels)) {
-            $post_type_obj->labels = new stdClass();
-            $post_type_obj->labels->name = 'dev/hack to fix WordPress Bug';
-        }
-    }
+	if ( !is_404() ){
+		if ( $wp_query->is_post_type_archive && $wp_query->is_tax ) {
+			global $post_type_obj;
+			$wp_query->is_tax = false;
+			$post_type_obj = get_queried_object();
+			if (empty($post_type_obj->labels)) {
+				$post_type_obj->labels = new stdClass();
+				$post_type_obj->labels->name = 'dev/hack to fix WordPress Bug';
+			}
+		}
+	}
 }add_action( 'parse_query', 'wordpress_fix_bug' );
