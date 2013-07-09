50158448cfe6ab5e4e61aeada5fffc4287895182
Phil Johnston <johnstonphilip@gmail.com>
2013-07-09 12:30:39 -0400

    beta1.0.6.0
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- mp-core.php
+++ mp-core.php
@@ -3,7 +3,7 @@
 Plugin Name: Move Plugins Core
 Plugin URI: http://moveplugins.com
 Description: A core group of classes and functions.
-Version: beta1.0.5.9
+Version: beta1.0.6.0
 Author: Move Plugins
 Author URI: http://moveplugins.com
 Text Domain: mp_core
