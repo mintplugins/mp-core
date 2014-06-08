7e9419a4d094f1538c8c2a46dc1b737142f78da3
Phil Johnston <johnstonphilip@gmail.com>
2014-06-08 01:22:27 -0400

    beta1.1.5.7
    Better apostrophe sanitization includes double quotes
    
    Signed-off-by: Phil Johnston <johnstonphilip@gmail.com>

--- mp-core.php
+++ mp-core.php
@@ -3,7 +3,7 @@
 Plugin Name: MP Core
 Plugin URI: http://mintplugins.com
 Description: A core group of classes and functions.
-Version: beta1.1.5.6
+Version: beta1.1.5.7
 Author: Mint Plugins
 Author URI: http://mintplugins.com
 Text Domain: mp_core
