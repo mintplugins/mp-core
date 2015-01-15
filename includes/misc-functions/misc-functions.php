40488f22e6ba21d073377240ea196c5a44aeae1a
Phil Johnston <support@mintplugins.com>
2015-01-15 17:05:03 -0500

    Simple Page HTML for single action added

--- includes/misc-functions/misc-functions.php
+++ includes/misc-functions/misc-functions.php
@@ -841,3 +841,155 @@ function mp_core_open_graph_video_meta_tags( $video_url ){
 	
 	return $content_output;
 }
+
+/**
+ * Return the HTML for a simple page where the user can make a decision or carry out an action
+ *
+ * @access   public
+ * @since    1.0.0
+ * @param    $date video_url A url to a video
+ * @return   $meta_tagts string the meta tags which should be placed in the header to make Open Graph display a video correctly.
+ */
+function mp_core_simple_action_page( $args ){
+	
+	$default_args = array(
+		'page_title' => NULL,
+		'h2_title' => NULL,
+		'page_body_html' => NULL,
+	);
+	
+	//Parse the args
+	$args = wp_parse_args( $args, $default_args );
+	
+	ob_start(); ?>
+    
+	<html xmlns="http://www.w3.org/1999/xhtml" lang="en-US"><head>
+		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+		<title><?php echo $args['page_title']; ?></title>
+		<style type="text/css">
+			html {
+				background: #f1f1f1;
+			}
+			body {
+				background: #fff;
+				color: #444;
+				font-family: "Open Sans", sans-serif;
+				margin: 2em auto;
+				padding: 1em 2em;
+				max-width: 700px;
+				-webkit-box-shadow: 0 1px 3px rgba(0,0,0,0.13);
+				box-shadow: 0 1px 3px rgba(0,0,0,0.13);
+			}
+			h1 {
+				border-bottom: 1px solid #dadada;
+				clear: both;
+				color: #666;
+				font: 24px "Open Sans", sans-serif;
+				margin: 30px 0 0 0;
+				padding: 0;
+				padding-bottom: 7px;
+			}
+			#error-page {
+				margin-top: 50px;
+			}
+			#error-page p {
+				font-size: 14px;
+				line-height: 1.5;
+				margin: 25px 0 20px;
+			}
+			#error-page code {
+				font-family: Consolas, Monaco, monospace;
+			}
+			ul li {
+				margin-bottom: 10px;
+				font-size: 14px ;
+			}
+			a {
+				color: #21759B;
+				text-decoration: none;
+			}
+			a:hover {
+				color: #D54E21;
+			}
+			.button {
+				background: #f7f7f7;
+				border: 1px solid #cccccc;
+				color: #555;
+				display: inline-block;
+				text-decoration: none;
+				font-size: 13px;
+				line-height: 26px;
+				height: 28px;
+				margin: 0;
+				padding: 0 10px 1px;
+				cursor: pointer;
+				-webkit-border-radius: 3px;
+				-webkit-appearance: none;
+				border-radius: 3px;
+				white-space: nowrap;
+				-webkit-box-sizing: border-box;
+				-moz-box-sizing:    border-box;
+				box-sizing:         border-box;
+	
+				-webkit-box-shadow: inset 0 1px 0 #fff, 0 1px 0 rgba(0,0,0,.08);
+				box-shadow: inset 0 1px 0 #fff, 0 1px 0 rgba(0,0,0,.08);
+				vertical-align: top;
+			}
+	
+			.button.button-large {
+				height: 29px;
+				line-height: 28px;
+				padding: 0 12px;
+			}
+	
+			.button:hover,
+			.button:focus {
+				background: #fafafa;
+				border-color: #999;
+				color: #222;
+			}
+	
+			.button:focus  {
+				-webkit-box-shadow: 1px 1px 1px rgba(0,0,0,.2);
+				box-shadow: 1px 1px 1px rgba(0,0,0,.2);
+			}
+	
+			.button:active {
+				background: #eee;
+				border-color: #999;
+				color: #333;
+				-webkit-box-shadow: inset 0 2px 5px -3px rgba( 0, 0, 0, 0.5 );
+				box-shadow: inset 0 2px 5px -3px rgba( 0, 0, 0, 0.5 );
+			}
+			input{
+				
+				border: 1px solid #ddd;
+				-webkit-box-shadow: inset 0 1px 2px rgba(0,0,0,.07);
+				box-shadow: inset 0 1px 2px rgba(0,0,0,.07);
+				background-color: #fff;
+				color: #333;
+				outline: 0;
+				-webkit-transition: .05s border-color ease-in-out;
+				transition: .05s border-color ease-in-out;
+				padding: 5px 10px;
+				font-size: 1em;
+				outline: 0;
+				width:100%;
+					
+			}
+	
+	</style>
+	</head>
+	<body id="error-page">
+		<p><h2><?php echo $args['h2_title']; ?></h2></p>
+		
+		<?php echo $args['page_body_html']; ?>
+		
+	</body></html>
+    
+    <?php $output_html = ob_get_contents(); 
+	
+	ob_end_clean();
+	
+	return $output_html;
+}
