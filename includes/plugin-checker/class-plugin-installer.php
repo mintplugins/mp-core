c9195d59523ca43454fc9ad8bc7407b4f63fc0cd
Phil Johnston <support@mintplugins.com>
2015-04-02 10:57:51 -0400

    Only show "Successfully Installed" if it was.

--- includes/plugin-checker/class-plugin-installer.php
+++ includes/plugin-checker/class-plugin-installer.php
@@ -277,10 +277,7 @@ if ( !class_exists( 'MP_CORE_Plugin_Installer' ) ){
 						
 			//Delete the temp zipped file
 			$wp_filesystem->rmdir($filename);
-							
-			//Display a successfully installed message
-			echo '<p>' . __( 'Successfully Installed ', 'mp_core' ) .  $this->_args['plugin_name']  . '</p>';
-					
+												
 			//If we are installing a theme
 			if ( $this->_args['plugin_is_theme'] ){
 				
@@ -307,6 +304,9 @@ if ( !class_exists( 'MP_CORE_Plugin_Installer' ) ){
 					}
 					
 				}
+				
+				//Display a successfully installed message
+				echo '<p>' . __( 'Successfully Installed ', 'mp_core' ) .  $this->_args['plugin_name']  . '</p>';
 					
 			}
 			//If we are installing a plugin
@@ -316,7 +316,19 @@ if ( !class_exists( 'MP_CORE_Plugin_Installer' ) ){
 				wp_cache_set( 'plugins', NULL, 'plugins' );
 			
 				//Activate plugin
-				print_r ( activate_plugin( trailingslashit( $upload_dir ) . $this->plugin_name_slug . '/' . $this->_args['plugin_filename'] ) );
+				$result = activate_plugin( trailingslashit( $upload_dir ) . $this->plugin_name_slug . '/' . $this->_args['plugin_filename'] );
+				
+				//If there was a problem installing the plugin
+				if ( is_wp_error( $result ) ) {
+					//Display an error message
+					echo '<p>' . __( 'Error Installing ', 'mp_core' ) .  $this->_args['plugin_name']  . '</p>';
+					echo '<p>' . $result->get_error_message() . '</p>';
+				}
+				//If we activated the plugin and it's all good
+				else{
+					//Display a successfully installed message
+					echo '<p>' . __( 'Successfully Installed ', 'mp_core' ) .  $this->_args['plugin_name']  . '</p>';
+				}
 			}
 		
 			if ( !empty( $this->_args['plugin_success_link'] ) ){
