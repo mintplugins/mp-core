db63a5c3678bbff4c2548dbd7f1980a3d69b3e3d
Phil Johnston <johnstonphilip@gmail.com>
2020-11-26 16:20:06 -0500

    Add alt tag to video positioning image

--- includes/misc-functions/template-tags/template-tags.php
+++ includes/misc-functions/template-tags/template-tags.php
@@ -325,7 +325,7 @@ function mp_core_oembed_get($video_url, $args = NULL){
 	$html_output .= !empty( $args['min_width'] ) ? ' min-width:' . $args['min_width'] . 'px; margin: 0px auto 0px auto;' : NULL;
 	$html_output .= !empty( $args['max_width'] ) ? ' max-width:' . $args['max_width'] . 'px; margin: 0px auto 0px auto;' : NULL;
 	$html_output .= '">';
-		$html_output .= '<img class="mp-core-oembed-full-width-img" style="position:relative; display:block; padding:0px; margin:0px; width:100%; border:none;';
+		$html_output .= '<img alt="Video positioning image" class="mp-core-oembed-full-width-img" style="position:relative; display:block; padding:0px; margin:0px; width:100%; border:none;';
 		$html_output .= '" width="100%" src="' . plugins_url( 'images/16x9.png', dirname(dirname(__FILE__))) . '"/>';
 		$html_output .= $iframe_code;
 	$html_output .= '</div>';
